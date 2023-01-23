7ac2ebc54d61082f7e57a7b2116aed3e99d5e4c2
IEscher <54139119+IEscher@users.noreply.github.com>
2023-01-23 15:29:20 +0100

    push push

--- docker/image-musician/src/app.js
+++ docker/image-musician/src/app.js
@@ -1,4 +1,23 @@
+const protocol = require('./concert-protocol');
 
-var protocol = require('./concert-protocol');
+// We use a standard Node.js module to work with UDP
+const dgram = ('dgram');
 
-console.log("Using multicast address: " + protocol.PROTOCOL_MULTICAST_ADDRESS + " and port: " + protocol.PROTOCOL_PORT);
+// Let's create a datagram socket. We will use it to send our UDP datagrams
+const s = dgram.createSocket('udp4');
+
+// Create a measure object and serialize it to JSON
+const measure = {
+    timestamp: Date.now(),
+    location: this.location,
+    temperature: this.temperature
+};
+const payload = JSON.stringify(measure);
+
+// Send the payload via UDP (multicast)
+message = new Buffer(payload);
+
+s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS,
+    function (err, bytes) {
+        console.log("Sending payload: " + payload + " via port " + s.address().port);
+    });
